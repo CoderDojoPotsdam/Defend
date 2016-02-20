2b257c2b1a0bf8cbf4e8a0b03277bc252ebc22df
derFogel <lieven.hey@snafu.de>
2016-02-20 17:02:07 +0100

    Controller erstellt

---
+++ controller.py
@@ -0,0 +1,13 @@
+# controller
+
+class Controller:
+	def __init__(self):
+		pass
+
+	def __del__(self):
+		pass
+
+	def update(self, command, **kwargs):
+		# update something
+		# Beispiel: controller.update("minion", "goto", [0,4, 0,2])
+		pass
