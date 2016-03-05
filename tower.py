6d91b2127c06a23e6caef54aef9cb93771eeb680
leonhaag <leonhaag10@gmail.com>
2016-03-05 15:33:37 +0100

    Toe wer

--- tower.py
+++ tower.py
@@ -1,4 +1,11 @@
-class Tower(object):
-    def __init__():
-        name = "bla"
-        health = 100
+class Tower:
+    def __init__(self):
+		self.name = "Tower"
+		self.__health = 100
+
+
+	def update(self,*args):
+		if args[0]=="hit":
+			self.__health -=args[1]
+			
+		
