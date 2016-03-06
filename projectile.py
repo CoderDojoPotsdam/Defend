95acdd6bdf719c2d27d0f4b7eba1448eaf56367f
0-zM <zeiske.milan@gmx.de>
2016-03-06 13:16:02 +0100

    kleiner Fehler

--- projectile.py
+++ projectile.py
@@ -5,7 +5,7 @@ class Projectile(object):
 		self.__motion = [0,0]
 		self.__damage = damage
 		self.__position = [0,0]
-	def update(self,args):
+	def update(self,*args):
 		if args[0] == "move":
 			for i in range (self.__position.len):
 				self.__position[i] +=self.__motion[i]
