3b28c1a902fdb702d99f37d1654a31afa6b43c55
0-zM <zeiske.milan@gmx.de>
2016-03-06 13:14:10 +0100

    Update projectile.py

--- projectile.py
+++ projectile.py
@@ -4,4 +4,8 @@ class Projectile(object):
 		self.__type = ptype
 		self.__motion = [0,0]
 		self.__damage = damage
-		
+		self.__position = [0,0]
+	def update(self,args):
+		if args[0] == "move":
+			for i in range (self.__position.len):
+				self.__position[i] +=self.__motion[i]
