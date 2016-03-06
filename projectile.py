28f8bd140534ec1dda2c4ffcf5da6c88740824fd
0-zM <zeiske.milan@gmx.de>
2016-03-06 13:29:40 +0100

    Update projectile.py

--- projectile.py
+++ projectile.py
@@ -6,6 +6,7 @@ class Projectile(object):
 		self.__motion = [0,0]
 		self.__damage = damage
 		self.__position = [0,0]
+		self.image = Image(source = "icons/projectile.xcf")
 	def update(self,*args):
 		if args[0] == "move":
 			for i in range (self.__position.len):
